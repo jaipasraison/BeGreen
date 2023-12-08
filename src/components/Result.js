35c07785ca7a4328a3d3a809b487728d62bf536a
Lyrastico <louisbarthes1@gmail.com>
2023-12-08 04:35:22 +0100

    indentation

--- src/components/Result.js
+++ src/components/Result.js
@@ -11,13 +11,13 @@ const Result = () => {
   } else {
     console.log('No data found in localStorage.');
   }
-  const waterUsedInShower = result[1][10]*52*result[1][10]*10
-  const waterUsedInBath = result[1][12]*150
-const waterUsedInBathroom = waterUsedInShower+waterUsedInBath
-const wateUsedInToiletPaper = result[2][14]*52*0.2
-const waterUsedInToiletFlush = result[2][13]*52*10
-const waterUsedInToilet = wateUsedInToiletPaper+waterUsedInToiletFlush
- console.log(waterUsedInBathroom)
+  const waterUsedInShower = result[1][10] * 52 * result[1][10] * 10
+  const waterUsedInBath = result[1][12] * 150
+  const waterUsedInBathroom = waterUsedInShower + waterUsedInBath
+  const wateUsedInToiletPaper = result[2][14] * 52 * 0.2
+  const waterUsedInToiletFlush = result[2][13] * 52 * 10
+  const waterUsedInToilet = wateUsedInToiletPaper + waterUsedInToiletFlush
+  console.log(waterUsedInBathroom)
 
   // Assuming 'result' has specific properties you want to display
   // For example, if it has a 'formResponses' key
@@ -27,7 +27,7 @@ const waterUsedInToilet = wateUsedInToiletPaper+waterUsedInToiletFlush
   return (
     <div>
       {waterUsedInBathroom}
-      
+
     </div>
   );
 };
