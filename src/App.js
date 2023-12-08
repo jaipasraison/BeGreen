195ab7214f5cb49974c8bdc3f0b2044bc10db5bb
AlexDesout <alexdesout@outlook.fr>
2023-12-08 07:37:44 +0100

    fixed the israeli-palestinian conflict

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-// import FormComponent from './components/FormPageTest';
-// import HomePage from './components/HomePage'
-// import './styles/App.css';
+// App.js
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import FormComponent from './components/FormPageTest';
+import QuizzFormComponent from './components/quizz/QuizzFormComponent';
+import Result from './components/Result';
+import './styles/index.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <HomePage />
-//     </div>
-//   );
-// }
+const routes = [
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/form',
+    element: <FormComponent current_page={1} />,
+  },
+  {
+    path: '/quizz/:theme',
+    element: <QuizzFormComponent />,
+  },
+  {
+    path: '/result',
+    element: <Result />,
+  },
+];
 
-// export default App;
+const router = createBrowserRouter(routes);
+
+const App = () => (
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
+// index.js (ou index.jsx)
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import FormComponent from './components/FormPageTest';
-import QuizzFormComponent from './components/quizz/QuizzFormComponent';
-import Result from './components/Result'
-import HomePage from './components/HomePage';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage/>
-  },
-  {
-    path: "/form",
-    element: <FormComponent current_page={1} />
-  },
-  {
-    path: "/quizz/:theme",
-    element: <QuizzFormComponent />
-  },
-  {
-    path: "/result",
-    element: <Result />
-  }
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <RouterProvider router={router} />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
