62dbe47e929b5a402659d2dde7ca2bbe151ca696
AlexDesout <alexdesout@outlook.fr>
2023-12-08 03:58:05 +0100

    get local storage data

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,6 +5,14 @@ import '../styles/form.css';
 import { useParams } from 'react-router';
 
 const Result = (state) => {
+
+    const storedDataString = localStorage.getItem('formResult');
+if (storedDataString) {
+  const storedData = JSON.parse(storedDataString);
+  console.log(storedData);
+} else {
+  console.log('Aucune donnée trouvée dans le localStorage.');
+}
     const result = state.formResult
     console.log (state)
     console.log(result)
